875d5c6a0e8051374a2eff91dba34af258ca947a
Marat <Marat_Markosian@epam.com>
2022-05-26 20:11:25 +0300

    Init files

---
+++ FitYou/FitYou/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitYou
+//
+//  Created by Марат Маркосян on 26.05.2022.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
